65f9fa7c7548d8c82ba621a19b80f3f9ff162f6f
Nova Dev <web3dev17@gmail.com>
2026-03-07 10:13:53 -0800

    cors settings implementation

--- template/src/server.ts
+++ template/src/server.ts
@@ -1,3 +1,4 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -7,7 +8,19 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+/*
+ CORS Configuration
+*/
+const corsOptions: cors.CorsOptions = {
+  origin: [
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", router);
